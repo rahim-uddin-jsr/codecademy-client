7d703c9924b0de96bbfe2069079f05f6311d6545
dev-rahim <rahimuddin5915@gmail.com>
2023-03-25 11:58:53 +0600

    logout functionality added

--- src/components/shared/Navigation/Navigation.js
+++ src/components/shared/Navigation/Navigation.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from "../../../images/icons8-learning-64.png";
 import { FaUserAlt } from "react-icons/fa";
+import { AuthContext } from '../../../contexts/UserContext/UserContext';
 
 const Navigation = () => {
+    const { user, logOut } = useContext(AuthContext)
+    const handleLogout = () => {
+        logOut().then((result) => {
+            alert('logOut successful')
+            console.log(user);
+        }).catch(error => {
+            alert(error.message)
+        })
+    }
     return (
         <div>
             <div className="navbar bg-base-100">
@@ -20,36 +30,42 @@ const Navigation = () => {
                         <li><NavLink to='/register'>Register</NavLink></li>
                     </ul>
                 </div>
-                <div className="dropdown dropdown-end">
-                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                        <div className="">
-                            <FaUserAlt className='text-3xl' />
-                        </div>
-                    </label>
-                    <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-                        <li>
-                            <NavLink to='/login' className="justify-between">
-                                Login
-                            </NavLink>
-                        </li>
-                        <li><NavLink to='/register'>Register</NavLink></li>
-                    </ul>
-                </div>
-                <div className="dropdown dropdown-end">
-                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                            <img src="https://picsum.photos/200" alt="THIS IS USER PROFILE IMAGE" />
-                        </div>
-                    </label>
-                    <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-                        <li>
-                            <NavLink to='/profile' className="justify-between">
-                                Profile
-                            </NavLink>
-                        </li>
-                        <li><a>Logout</a></li>
-                    </ul>
-                </div>
+                {/* user Profile picture shown */}
+                {!user?.uid ?
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                            <div className="">
+                                <FaUserAlt className='text-3xl' />
+                            </div>
+                        </label>
+                        <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                            <li>
+                                <NavLink to='/login' className="justify-between">
+                                    Login
+                                </NavLink>
+                            </li>
+                            <li><NavLink to='/register'>Register</NavLink></li>
+                        </ul>
+                    </div>
+                    :
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img src="https://picsum.photos/200" alt="THIS IS USER PROFILE IMAGE" />
+                            </div>
+                        </label>
+                        <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                            <li>
+                                <NavLink to='/profile' className="justify-between">
+                                    Profile
+                                </NavLink>
+                            </li>
+                            <li><NavLink onClick={handleLogout}>Logout</NavLink></li>
+                        </ul>
+                    </div>
+                }
+
+
             </div>
         </div>
     );
