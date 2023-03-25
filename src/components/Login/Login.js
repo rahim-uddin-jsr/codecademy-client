041aa4a36048ae9b905a55c7ad018ba5e7a17dbb
dev-rahim <rahimuddin5915@gmail.com>
2023-03-25 11:51:23 +0600

    email password login functionality added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
+import { AuthContext } from '../../contexts/UserContext/UserContext';
 
 const Login = () => {
+    const { user, loginWithEmailPass } = useContext(AuthContext)
+    const emailRef = useRef('')
+    const passwordRef = useRef('')
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email, password);
+        loginWithEmailPass(email, password)
+            .then((result) => {
+                alert('user login successful')
+            }).catch(error => {
+                alert(error.message)
+            })
+    }
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -9,26 +25,26 @@ const Login = () => {
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
+                    <form onSubmit={handleSubmit} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input ref={emailRef} type="email" placeholder="email" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input ref={passwordRef} type="text" placeholder="password" className="input input-bordered" />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <button type='submit' className="btn  btn-primary">Login</button>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
